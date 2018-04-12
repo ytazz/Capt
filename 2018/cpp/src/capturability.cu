52824d18dce6409f2d44fc18f781991b9381c1cd
dio <dio@dio.dio>
2018-04-12 18:18:52 +0900

    ;

--- 2018/cpp/src/capturability.cu
+++ 2018/cpp/src/capturability.cu
@@ -1,7 +1,40 @@
-struct Robots {
+/**
+   \author GWANWOO KIM
+ */
+#include "capturability.h"
+#include "tools.h"
 
-};
+using namespace std;
+
+// struct RobotsParam {
+//     const float heightOfCOM = 0.50;
+//     const float omega = sqrt(9.81/heightOfCOM);
+//
+//     const float step_max[2] = {0.3, 0.3};
+//     const float step_min[2] = {-0.3, -0.3};
+//
+//     const float cp_max[2] = {0.2, 0.2};
+//     const float cp_min[2] = {-0.2, -0.2};
+//
+//     const int numGrid = 100;
+//
+//     __device__ float take_a_step( float *currentSwFtPos, float *desiredSwFtPos) {
+//     }
+// };
+//
+// __global__ void kernel
+//
+//
+// void setStateSpace( float *stateSpace, float *cp_max,
+//                     float *cp_min, float *step_max, float *step_min)
+
+
 
 int main (void) {
+  vector<double> test;
+  test = linspace(0.0, 2.0, 100);
+  for(int i = 0; i< 100; i++){
+    printf("%lf",test[i]);
+  }
   return 0;
 }
---
+++ 2018/cpp/src/tools.h
@@ -0,0 +1,17 @@
+#ifndef __tools_H__
+#define __tools_H__
+#include <stdio.h>
+
+template <typename T>
+std::vector<T> linspace(T a, T b, size_t N) {
+    T h = (b - a) / static_cast<T>(N-1);
+    std::vector<T> xs(N);
+    typename std::vector<T>::iterator x;
+    T val;
+    for (x = xs.begin(), val = a; x != xs.end(); ++x, val += h)
+        *x = val;
+    return xs;
+}
+
+#else
+#endif

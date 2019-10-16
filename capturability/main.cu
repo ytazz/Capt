24f84fece428afc07123a92c5355c39bdbb68bd7
dl-box <>
2019-10-16 15:18:32 +0900

    GPU Analysis
    rename test.cu -> main.cu

--- capturability/main.cu
+++ capturability/main.cu
@@ -1,55 +1,174 @@
-#include "analysis_gpu.cuh"
+#include "cuda_analysis.cuh"
 
-const int BPG = 1024; // Blocks  Per Grid  (max: 65535)
-const int TPB = 1024; // Threads Per Block (max: 1024)
+const int BPG = 65535; // Blocks  Per Grid  (max: 65535)
+const int TPB = 1024;  // Threads Per Block (max: 1024)
+
+// typedef Cuda::GridCartesian grid_t;
+typedef Cuda::GridPolar grid_t;
 
 int main(void) {
+  /* 前処理 */
+  /* ---------------------------------------------------------------------- */
+  printf("Prepare...\t");
+
   /* パラメータの読み込み */
-  Model model("nao.xml");
-  Param param("analysis.xml");
+  Capt::Model cmodel("nao.xml");
+  Capt::Param cparam("analysis.xml");
 
   /* グリッド */
+  Capt::Grid cgrid(cparam);
+  const int  num_state = cgrid.getNumState();
+  const int  num_input = cgrid.getNumInput();
+  const int  num_grid  = num_state * num_input;
+
+  /* 解析条件 */
+  Cuda::Condition cond;
+  cond.model = &cmodel;
+  cond.param = &cparam;
+  cond.grid  = &cgrid;
+
+  /* メモリー管理 */
+  Cuda::MemoryManager mm;
+
+  /* パラメータの用意 */
   // ホスト側
-  Grid *grid;
-  grid = new Grid(param);
+  Cuda::State   *state   = (Cuda::State*)malloc(sizeof( Cuda::State ) * num_state);
+  Cuda::Input   *input   = (Cuda::Input*)malloc(sizeof( Cuda::Input ) * num_input );
+  int           *trans   = (int*)malloc(sizeof( int ) * num_state * num_input );
+  int           *basin   = (int*)malloc(sizeof( int ) * num_state );
+  int           *nstep   = (int*)malloc(sizeof( int ) * num_grid );
+  grid_t        *grid    = new grid_t;
+  Cuda::Vector2 *cop     = new Cuda::Vector2[num_state];
+  Cuda::Physics *physics = new Cuda::Physics;
+  mm.initHostState(state, cond);
+  mm.initHostTrans(trans, cond);
+  mm.initHostInput(input, cond);
+  mm.initHostBasin(basin, cond);
+  mm.initHostNstep(nstep, cond);
+  mm.initHostGrid(grid, cond);
+  mm.initCop(cop, cond);
+  mm.initPhysics(physics, cond);
+  mm.setGrid(grid);
   // デバイス側
-  Grid *dev_grid;
-  HANDLE_ERROR(cudaMalloc((void **)&dev_grid, sizeof(Grid)));
+  Cuda::State   *dev_state;
+  Cuda::Input   *dev_input;
+  int           *dev_basin;
+  int           *dev_nstep;
+  int           *dev_trans;
+  grid_t        *dev_grid;
+  Cuda::Vector2 *dev_cop;
+  Cuda::Physics *dev_physics;
+  // mm.initDevState(dev_state);
+  // mm.initDevInput(dev_input);
+  // mm.initDevTrans(dev_trans);
+  // mm.initDevBasin(dev_basin);
+  // mm.initDevNstep(dev_nstep);
+  // mm.initDevGrid(dev_grid);
+  // mm.initDevCop(dev_cop);
+  // mm.initDevPhysics(dev_physics);
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_state, num_state * sizeof( Cuda::State ) ) );
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_input, num_input * sizeof( Cuda::Input ) ) );
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_trans, num_grid * sizeof( int ) ) );
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_basin, num_state * sizeof( int ) ) );
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_nstep, num_grid * sizeof( int ) ) );
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_grid, sizeof( grid_t ) ) );
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_cop, num_state * sizeof( Cuda::Vector2 ) ) );
+  HANDLE_ERROR(cudaMalloc( (void **)&dev_physics, sizeof( Cuda::Physics ) ) );
+
   // ホスト側からデバイス側にコピー
-  HANDLE_ERROR(
-      cudaMemcpy(dev_grid, grid, sizeof(Grid), cudaMemcpyHostToDevice));
+  mm.copyHostToDevState(state, dev_state);
+  mm.copyHostToDevInput(input, dev_input);
+  mm.copyHostToDevTrans(trans, dev_trans);
+  mm.copyHostToDevBasin(basin, dev_basin);
+  mm.copyHostToDevNstep(nstep, dev_nstep);
+  mm.copyHostToDevGrid(grid, dev_grid);
+  mm.copyHostToDevCop(cop, dev_cop);
+  mm.copyHostToDevPhysics(physics, dev_physics);
 
-  // グリッド数の確認
-  const int num_data = grid->getNumState() * grid->getNumInput();
-  std::cout << "num_state = " << grid->getNumState() << '\n';
-  std::cout << "num_input = " << grid->getNumInput() << '\n';
+  printf("Done.\n");
+  /* ------------------------------------------------------------------------ */
 
-  /* 解析結果保存用変数 */
-  // ホスト側
-  Capturability *capturability;
-  capturability = new Capturability(model, param);
-  // デバイス側
-  Capturability *dev_capturability;
-  HANDLE_ERROR(cudaMalloc((void **)&dev_capturability, sizeof(Capturability)));
-  // ホスト側からデバイス側にコピー
-  HANDLE_ERROR(cudaMemcpy(dev_capturability, capturability,
-                          sizeof(Capturability), cudaMemcpyHostToDevice));
+  /* 状態遷移計算 */
+  /* ---------------------------------------------------------------------- */
+  printf("Calculate...\t");
 
-  /* 解析 */
-  exeZero<<<BPG, TPB>>>(dev_capturability, dev_grid);
+  Cuda::calcStateTrans << < BPG, TPB >> > ( dev_state, dev_input, dev_trans, dev_grid, dev_cop, dev_physics );
+  HANDLE_ERROR(cudaMemcpy(trans, dev_trans, num_grid * sizeof( int ),
+                          cudaMemcpyDeviceToHost) );
 
-  /* 結果 */
-  // デバイス側からホスト側にコピー
-  HANDLE_ERROR(cudaMemcpy(capturability, dev_capturability,
-                          sizeof(Capturability), cudaMemcpyDeviceToHost));
+  printf("Done.\n");
+  /* ---------------------------------------------------------------------- */
+
+  /* 解析実行 */
+  /* ---------------------------------------------------------------------- */
+  printf("Execute...\n");
+
+  printf("\t0-step\n");
+  Cuda::exeZeroStep(cgrid, cmodel, basin);
+  HANDLE_ERROR(cudaMemcpy(dev_basin, basin, num_state * sizeof( int ),
+                          cudaMemcpyHostToDevice) );
+
+  int  N    = 1;
+  bool flag = true;
+  while (flag) {
+    printf("\t%d-step\n", N);
+    Cuda::exeNStep << < BPG, TPB >> > ( N, dev_basin, dev_nstep,
+                                        dev_trans, dev_grid );
+    HANDLE_ERROR(cudaMemcpy(basin, dev_basin, num_state * sizeof( int ),
+                            cudaMemcpyDeviceToHost) );
+
+    if (N > 2)
+      flag = false;
+    N++;
+  }
+
+  printf("\t\tDone.\n");
+  /* ---------------------------------------------------------------------- */
+
+  /* 解析結果をデバイス側からホスト側にコピー */
+  /* ---------------------------------------------------------------------- */
+  HANDLE_ERROR(cudaMemcpy(basin, dev_basin, num_state * sizeof( int ),
+                          cudaMemcpyDeviceToHost) );
+  HANDLE_ERROR(cudaMemcpy(nstep, dev_nstep, num_grid * sizeof( int ),
+                          cudaMemcpyDeviceToHost) );
+  HANDLE_ERROR(cudaMemcpy(trans, dev_trans, num_grid * sizeof( int ),
+                          cudaMemcpyDeviceToHost) );
+  /* ---------------------------------------------------------------------- */
+
+  /* ファイル書き出し */
+  /* ---------------------------------------------------------------------- */
+  printf("Output...\t");
+  Cuda::outputBasin("BasinGpu.csv", cond, basin, false);
+  Cuda::outputNStep("NstepGpu.csv", cond, nstep, trans, false);
+  printf("Done.\n");
+  /* ---------------------------------------------------------------------- */
+
+  /* 終了処理 */
+  /* ---------------------------------------------------------------------- */
+  printf("Finish...\t");
 
   /* メモリの開放 */
   // ホスト側
+  delete state;
+  delete input;
+  delete basin;
+  delete nstep;
+  delete trans;
   delete grid;
-  delete capturability;
+  delete cop;
+  delete physics;
   // デバイス側
+  cudaFree(dev_state);
+  cudaFree(dev_input);
+  cudaFree(dev_trans);
+  cudaFree(dev_basin);
+  cudaFree(dev_nstep);
   cudaFree(dev_grid);
-  cudaFree(dev_capturability);
+  cudaFree(dev_cop);
+  cudaFree(dev_physics);
+
+  printf("Done.\n");
+  /* ---------------------------------------------------------------------- */
 
   return 0;
 }

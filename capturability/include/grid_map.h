27279f05e449478cfcc7e073582b51c82ad8a3cc
Hiroki Kuribayashi <kuribayashi@stu.kobe-u.ac.jp>
2019-10-30 16:55:45 +0900

    GridMap
    作成中

--- capturability/include/grid_map.h
+++ capturability/include/grid_map.h
@@ -1,14 +1,43 @@
 #ifndef __GRID_MAP_H__
 #define __GRID_MAP_H__
 
+#include "param.h"
+#include <vector>
+
 namespace Capt {
+
+enum Foot { RIGHT, LEFT };
+
+struct Node {
+  Node * parent;
+  int    state_id;
+  Foot   suf;
+  double cost;
+  int    step;
+};
+
+struct List {
+  void  add(Node* node);
+  Node* findMinCostNode();
+
+  std::vector<Node*> nodes;
+};
 
 class GridMap {
 public:
   GridMap();
   ~GridMap();
 
+  void init(Param param);
+
+  void clear();
+
+  void setNode(int i, int j, Node node);
+  std::vector<Node*> getNode(i, j);
+
 private:
+  Grid grid;
+  List open_list;
 };
 
 } // namespace Capt

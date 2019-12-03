94937dc4c84fa2878255d9a907c77cf32ef20eb3
dl-box <>
2019-12-03 13:33:58 +0900

    add: Planner
    雛形を作成

--- capturability/include/planner.h
+++ capturability/include/planner.h
@@ -12,17 +12,39 @@
 namespace planner {
 
 struct Input {
-  double       time;
+  double       elapsed_time;
+  Capt::Foot   suf;
   Capt::vec3_t rfoot;
   Capt::vec3_t lfoot;
   Capt::vec3_t icp;
+  Capt::vec3_t goal;
+
+  void operator=(const Input &input) {
+    this->cop          = input.cop;
+    this->swf          = input.swf;
+    this->elapsed_time = input.elapsed_time;
+    this->suf          = input.suf;
+    this->rfoot        = input.rfoot;
+    this->lfoot        = input.lfoot;
+    this->icp          = input.icp;
+    this->goal         = input.goal;
+  }
 };
 
 struct Output {
+  double       planning_time;
   Capt::vec3_t rfoot;
   Capt::vec3_t lfoot;
   Capt::vec3_t icp;
   Capt::vec3_t cop;
+
+  void operator=(const Output &output) {
+    this->planning_time = output.planning_time;
+    this->rfoot         = output.rfoot;
+    this->lfoot         = output.lfoot;
+    this->icp           = output.icp;
+    this->cop           = output.cop;
+  }
 };
 
 }
@@ -36,11 +58,18 @@ public:
 
   void            set(planner::Input input);
   planner::Output get();
+
 private:
   void run();
   void selectSupportFoot();
   void runSearch();
   void generatePath();
+
+  Tree   *tree;
+  Search *search;
+
+  planner::Input  input;
+  planner::Output output;
 };
 
 } // namespace Capt
--- capturability/src/planner.cpp
+++ capturability/src/planner.cpp
@@ -2,6 +2,20 @@
 
 using namespace Capt;
 
-Planner::Planner(Model *model, Param *param, Capturability *capt){
+Planner::Planner(Model *model, Param *param, Grid *grid, Capturability *capt){
+  tree   = new Tree(param, grid, capt);
+  search = new Search(grid, tree);
+}
 
+Planner::Planner(){
+  delete tree;
+  delete search;
+}
+
+void Planner::set(planner::Input input){
+  this->input = input;
+}
+
+planner::Output Planner::get(){
+  return this->output;
 }

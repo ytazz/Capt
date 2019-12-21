dd43283b92eddce83ac5c6f733c1deb34bfa1a2e
Hiroki Kuribayashi <kuribayashi@stu.kobe-u.ac.jp>
2019-12-21 17:25:00 +0900

    edit interpolation
    add Interp 2 & 3

--- capturability/include/interpolation.h
+++ capturability/include/interpolation.h
@@ -1,19 +1,44 @@
+/*
+
+   Interpolation calss
+
+   - 4 dim ( 5 parameters )
+     - x0, xf, v0, vf, tf
+   - 2 dim ( 3 parameters )
+     - x0, xf, tf
+
+ */
+
 #ifndef __INTERPOLATION_H__
 #define __INTERPOLATION_H__
 
 namespace Capt {
 
-class Interpolation {
+class Interp3 {
 public:
-  Interpolation();
-  ~Interpolation();
+  Interp3();
+  ~Interp3();
 
-  void set(const double xi_0, const double xi_f, const double d_xi_0,
-           const double d_xi_f, const double t_f);
+  void set(const double x0, const double xf, const double v0,
+           const double vf, const double tf);
   double get(const double t);
 
 private:
   double coef[4];
 };
+
+class Interp2 {
+public:
+  Interp2();
+  ~Interp2();
+
+  void   set(const double x0, const double xf, const double vf, const double vf, const double tf);
+  double get(const double t);
+
+private:
+  double coef[3];
+};
+
 }
+
 #endif
--- capturability/src/interpolation.cpp
+++ capturability/src/interpolation.cpp
@@ -2,21 +2,40 @@
 
 namespace Capt {
 
-Interpolation::Interpolation() {}
-
-Interpolation::~Interpolation() {}
-
-void Interpolation::set(const double xi_0, const double xi_f, const double d_xi_0,
-                        const double d_xi_f, const double t_f) {
-  coef[0] = xi_0;
-  coef[1] = d_xi_0;
-  coef[2] =
-      (1 / (t_f * t_f)) * (3 * (xi_f - xi_0) - (2 * d_xi_0 + d_xi_f) * t_f);
-  coef[3] =
-      (1 / (t_f * t_f * t_f)) * (-2 * (xi_f - xi_0) + (d_xi_0 + d_xi_f) * t_f);
+Interp3::Interp3() {
 }
 
-double Interpolation::get(const double t) {
+Interp3::~Interp3() {
+}
+
+void Interp3::set(const double x0, const double xf, const double v0,
+                  const double vf, const double tf) {
+  coef[0] = x0;
+  coef[1] = v0;
+  coef[2] =
+    ( 1 / ( tf * tf ) ) * ( 3 * ( xf - x0 ) - ( 2 * v0 + vf ) * tf );
+  coef[3] =
+    ( 1 / ( tf * tf * tf ) ) * ( -2 * ( xf - x0 ) + ( v0 + vf ) * tf );
+}
+
+double Interp3::get(const double t) {
   return coef[0] + coef[1] * t + coef[2] * t * t + coef[3] * t * t * t;
 }
+
+Interp2::Interp2() {
+}
+
+Interp2::~Interp2() {
+}
+
+void Interp2::set(const double x0, const double xf, const double vf, const double tf) {
+  coef[0] = x0;
+  coef[1] = ( 2 * ( xf - x0 ) - vf * tf ) / tf;
+  coef[2] = ( x0 - xf + vf * tf ) / ( tf * tf );
+}
+
+double Interp2::get(const double t) {
+  return coef[0] + coef[1] * t + coef[2] * t * t;
+}
+
 }
